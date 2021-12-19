745fa96fe9a98b3af43871b5d0a0bcad69385673
Ayabee527 <66703857+Ayabee527@users.noreply.github.com>
2021-12-19 13:54:58 +0000

    Inits

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,16 +1,32 @@
 package;
 
 import flixel.FlxState;
+import flixel.util.FlxSpriteUtil;
 
 class PlayState extends FlxState
 {
+	var player:Player;
+
+	var yMultiples = [
+		16, 32, 48, 64, 80, 96, 112, 128, 144, 160, 176, 192, 208, 224, 240, 256, 272, 288, 304, 320, 336, 352, 368, 384, 400, 416, 432, 448, 464
+	];
+	var xMultiples = [
+		16, 32, 48, 64, 80, 96, 112, 128, 144, 160, 176, 192, 208, 224, 240, 256, 272, 288, 304, 320, 336, 352, 368, 384, 400, 416, 432, 448, 464, 480, 496,
+		512, 528, 544, 560, 576, 592, 608, 624
+	];
+
 	override public function create()
 	{
 		super.create();
+
+		player = new Player();
+		add(player);
 	}
 
 	override public function update(elapsed:Float)
 	{
+		FlxSpriteUtil.bound(player);
+
 		super.update(elapsed);
 	}
 }
--- source/Player.hx
+++ source/Player.hx
@@ -1,16 +1,50 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
+import flixel.util.FlxColor;
 
 class Player extends FlxSprite
 {
-	override public function new()
+	var moveTimer:Float = 0;
+
+	override public function new(x:Float = 16, y:Float = 16)
 	{
-		super();
+		super(x, y);
+
+		makeGraphic(16, 16, FlxColor.WHITE);
+	}
+
+	function move(elapsed:Float)
+	{
+		var up = FlxG.keys.anyPressed([W, UP]);
+		var left = FlxG.keys.anyPressed([A, LEFT]);
+		var down = FlxG.keys.anyPressed([S, DOWN]);
+		var right = FlxG.keys.anyPressed([D, RIGHT]);
+
+		var delay = 0.1;
+
+		if (moveTimer > delay)
+		{
+			if (up)
+				y -= 16;
+			else if (left)
+				x -= 16;
+			else if (down)
+				y += 16;
+			else if (right)
+				x += 16;
+
+			moveTimer -= delay;
+		}
+
+		moveTimer += elapsed;
 	}
 
 	override function update(elapsed:Float)
 	{
+		move(elapsed);
+
 		super.update(elapsed);
 	}
 }
